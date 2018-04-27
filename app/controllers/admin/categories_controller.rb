a33f7275408da6cef78bce0b70de30e96a363a47
Jojo <jody.sekhon@gmail.com>
2018-04-27 19:42:48 +0000

    Added admin authentication on admin products controller, categories controller and dashboard

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['HTTP_ADMIN_NAME'], password: ENV['HTTP_ADMIN_PASSWORD']
     def index
       @categories = Category.order(name: :asc).all
     end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_ADMIN_NAME'], password: ENV['HTTP_ADMIN_PASSWORD']
   def show
   end
 end
